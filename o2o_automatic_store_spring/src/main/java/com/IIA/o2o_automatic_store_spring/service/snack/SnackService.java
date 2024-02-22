15891802438f848738d4643c6a42963b5b8dc01e
Jeyong <118044367+joon6093@users.noreply.github.com>
2024-02-22 23:52:23 +0900

    [UPDATE] RestTemplate이 아닌 webClient를 사용하도록 수정

--- o2o_automatic_store_spring/src/main/java/com/IIA/o2o_automatic_store_spring/service/snack/SnackService.java
+++ o2o_automatic_store_spring/src/main/java/com/IIA/o2o_automatic_store_spring/service/snack/SnackService.java
@@ -5,36 +5,38 @@ import com.IIA.o2o_automatic_store_spring.dto.snack.SnackDto;
 import com.IIA.o2o_automatic_store_spring.entity.snack.Position;
 import com.IIA.o2o_automatic_store_spring.entity.snack.Snack;
 import com.IIA.o2o_automatic_store_spring.exception.ImageReadException;
-import com.IIA.o2o_automatic_store_spring.exception.NullResponseFromApiException;
 import com.IIA.o2o_automatic_store_spring.exception.SnackNotFoundException;
 import com.IIA.o2o_automatic_store_spring.repository.snack.SnackRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+@Transactional
 public class SnackService {
 
     private final SnackRepository snackRepository;
+    private final WebClient webClient;
 
-    @Value("${spring.flask.base-url}")
-    private String flaskBaseUrl;
-
+    public SnackService(SnackRepository snackRepository, @Value("${spring.flask.base-url}") String flaskBaseUrl) {
+        this.snackRepository = snackRepository;
+        this.webClient = WebClient.builder()
+                .baseUrl(flaskBaseUrl)
+                .build();
+    }
     public List<Snack> findAllSnacks() {
         return snackRepository.findAll();
     }
@@ -44,13 +46,7 @@ public class SnackService {
                 () -> new SnackNotFoundException(id.toString()));
     }
 
-    @Transactional
     public List<Snack> analyzeSnacks(List<MultipartFile> images) {
-        String baseUrl = flaskBaseUrl + "/detect";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         images.forEach(image -> {
             try {
@@ -66,14 +62,14 @@ public class SnackService {
             }
         });
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-        ResponseEntity<List<SnackDto>> responseEntity;
-        try {
-            responseEntity= restTemplate.exchange(baseUrl, HttpMethod.POST, requestEntity, new ParameterizedTypeReference<List<SnackDto>>() {});
-        } catch (ResourceAccessException e) {
-            throw new NullResponseFromApiException();
-        }
-        List<SnackDto> snackDtos = responseEntity.getBody();
+        List<SnackDto> snackDtos = webClient.post()
+                .uri("/detect")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(body))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<SnackDto>>() {})
+                .block();
+
         List<Snack> snacks = snackDtos.stream()
                 .map(dto -> Snack.builder()
                         .filename(dto.getFilename())
@@ -81,7 +77,9 @@ public class SnackService {
                         .position(new Position(dto.getPosition().getX1(), dto.getPosition().getX2(), dto.getPosition().getY1(), dto.getPosition().getY2()))
                         .build())
                 .collect(Collectors.toList());
+
         snackRepository.saveAll(snacks);
         return snacks;
     }
 }
+
