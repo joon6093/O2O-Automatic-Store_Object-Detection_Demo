dc4d3b7a72eadf01136ab426c6198211af41d6ee
Jeyong <118044367+joon6093@users.noreply.github.com>
2024-02-22 23:53:00 +0900

    [INIT] 초기 환경 설정

---
+++ o2o_automatic_store_springwebflux_kotlin/src/main/kotlin/com/IIA/o2o_automatic_store_springwebflux_kotlin/O2oAutomaticStoreSpringwebfluxKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.IIA.o2o_automatic_store_springwebflux_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class O2oAutomaticStoreSpringwebfluxKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<O2oAutomaticStoreSpringwebfluxKotlinApplication>(*args)
+}
---
+++ o2o_automatic_store_springwebflux_kotlin/src/test/kotlin/com/IIA/o2o_automatic_store_springwebflux_kotlin/O2oAutomaticStoreSpringwebfluxKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.IIA.o2o_automatic_store_springwebflux_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class O2oAutomaticStoreSpringwebfluxKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
