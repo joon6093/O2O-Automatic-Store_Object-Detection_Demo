7d0a3e993e747e5df449417f4c9206ba637d132a
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-03-09 23:18:19 +0900

    [UPDATE] object detection 서버에서 검출 결과를 kafka topic으로 전송하도록 구현

--- MessageQueue/object-detection/demo.py
+++ MessageQueue/object-detection/demo.py
@@ -1,17 +1,32 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-from datetime import datetime
+from kafka import KafkaProducer
 import json
 import pytz
+from datetime import datetime
 from py_eureka_client import eureka_client
+import logging
 
 app = Flask(__name__)
 
-def register_service_to_eureka():
-    eureka_client.init(eureka_server="http://127.0.0.1:8761/eureka/",
-                       app_name="object-detection",
-                       instance_port=5000,
-                       instance_ip='127.0.0.1',)
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='demo.log',
+                    filemode='a')
+
+logger = logging.getLogger(__name__)
+
+eureka_client.init(eureka_server="127.0.0.1:8761/eureka/",
+                    app_name="object-detection",
+                    instance_port=5000,
+                    instance_ip='127.0.0.1',)
+
+kafka_producer = KafkaProducer(
+    bootstrap_servers="127.0.0.1:9092", 
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
+
+KAFKA_TOPIC = 'object-detection-results'
 
 def load_demo_results():
     with open('demo_results.json') as f:
@@ -50,21 +65,30 @@ def detect():
     if not store_id:
         return 'Store-ID header is missing', 400
     
-    print(f"Store-ID: {store_id}")
     images = request.files.getlist('images')
     demo_results = load_demo_results()
-    results = []
-
+    item_summary = {}
     for image_file in images:
         filename = secure_filename(image_file.filename)
-        
         for demo_result in demo_results:
-            demo_result_modified = demo_result.copy()  
-            demo_result_modified['filename'] = filename 
-            results.append(demo_result_modified)
+            item_name = demo_result['object_name']
+            if item_name in item_summary:
+                item_summary[item_name]['count'] += 1
+            else:
+                item_summary[item_name] = {'count': 1}
+        
+        kafka_message = {
+                'store_id': store_id,
+                'items': item_summary
+                }
+        try:
+            future = kafka_producer.send(topic=KAFKA_TOPIC, value=kafka_message)
+            future.add_callback(lambda metadata: logger.info(f"Message sent to {metadata.topic} partition {metadata.partition} file {filename}"))
+            future.add_errback(lambda e: logger.error(str(e)))
+        except Exception as e:
+            logger.error(f"Failed to send message to Kafka: {str(e)}")
 
-    return jsonify(results)
+    return jsonify({"status": "OK"}), 200
 
 if __name__ == '__main__':
-    register_service_to_eureka()
     app.run(debug=True, host='127.0.0.1', port=5000)
